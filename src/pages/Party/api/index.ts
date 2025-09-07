c85354f1df90b4fc88d67dfd904cd6f5efef12e0
holden <al1onggwp@gmail.com>
2025-09-07 10:40:54 +0500

    fix imports

--- src/pages/Party/api/index.ts
+++ src/pages/Party/api/index.ts
@@ -1,5 +1,18 @@
 import { request, apiUrl } from "@/shared/api/api.config";
 
+import type { IParty } from "@/shared/types/party";
+
+export async function getParty(url: string) {
+  try {
+    const partyResponse = (await request.get(url)).data;
+    return partyResponse.res.batches as IParty[];
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+
 export async function createBatch(payload: any) {
   try {
     const response = await request.post(
